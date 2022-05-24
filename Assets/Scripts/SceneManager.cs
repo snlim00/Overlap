a68cf19f51c2414d7c82237b0e7c66a75961feb8
Choi Ui Man <snlim00@naver.com>
2022-05-24 14:47:04 +0900

    - 씬 매니저 및 씬 이동 제작 중

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager S = null;
+
+    private void Awake()
+    {
+        S = this;
+    }
+
+    public void ChangeScene()
+    {
+
+    }
+
+    public void MainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SCENE.MAIN_MENU);
+        
+    }
+
+    public void StartGame(string levelName, int dif)
+    {
+        Level.S.ReadLevel(levelName, dif);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SCENE.GAME_SCENE);
+    }
+
+    public void ResultScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SCENE.RESULT_SCENE);
+
+        //GameInfo.S.ShowResult();
+    }
+
+    public void Retry()
+    {
+        StartGame(Level.S.levelName, Level.S.levelDifficulty);
+    }
+}
